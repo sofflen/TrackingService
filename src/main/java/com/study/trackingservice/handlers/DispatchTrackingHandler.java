079074f92806ca7f54268513de80c6eed4dde0cd
sofflen <nixxesgaming@gmail.com>
2024-10-06 12:22:18 +0300

    updated handler to be able to handle multiple event types

--- src/main/java/com/study/trackingservice/handlers/DispatchTrackingHandler.java
+++ src/main/java/com/study/trackingservice/handlers/DispatchTrackingHandler.java
@@ -4,24 +4,26 @@ import com.study.dispatchservice.messages.DispatchPreparingEvent;
 import com.study.trackingservice.services.TrackingService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
+@KafkaListener(
+        id = "dispatchTrackingConsumerClient",
+        topics = DispatchTrackingHandler.DISPATCH_TRACKING_TOPIC,
+        groupId = "tracking.dispatch.tracking.consumer",
+        containerFactory = "kafkaListenerContainerFactory"
+)
 public class DispatchTrackingHandler {
 
     public static final String DISPATCH_TRACKING_TOPIC = "dispatch.tracking";
 
     private final TrackingService trackingService;
 
-    @KafkaListener(
-            id = "dispatchTrackingConsumerClient",
-            topics = DISPATCH_TRACKING_TOPIC,
-            groupId = "tracking.dispatch.tracking.consumer",
-            containerFactory = "kafkaListenerContainerFactory"
-    )
+    @KafkaHandler
     public void listen(DispatchPreparingEvent payload) {
         log.info("DispatchTrackingHandler received payload: {}", payload);
 
